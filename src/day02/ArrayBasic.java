d88c7797a0273cd8bc85b06887b76fddee489fb1
Jehoon <limjhoon8@gmail.com>
2024-04-08 10:33:16 +0900

    #파일 업데이트

--- src/day02/ArrayBasic.java
+++ src/day02/ArrayBasic.java
@@ -5,5 +5,16 @@ public class ArrayBasic {
     public static void main(String[] args) {
 
 
+        // 2. 크기가 불변함: 크기를 동적으로 늘이거나 줄일 수 없음.
+
+        // 배열의 생성 순서
+
+        // 1. 배열 변수 선언
+        int[] numbers;
+
+        // 2. 배열의 객체를 생성
+        numbers = new int[];
+
+
     }
 }
