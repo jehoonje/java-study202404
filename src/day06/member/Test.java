6e83e93d0b50fac4f9883f50a8785ae47e732260
Jehoon <limjhoon8@gmail.com>
2024-04-16 15:14:56 +0900

    #파일 업데이트

--- src/day06/member/Test.java
+++ src/day06/member/Test.java
@@ -1,6 +1,6 @@
 package day06.member;
 
-import java.util.scanner;
+import java.util.Scanner;
 
 public class Test {
 
@@ -8,8 +8,42 @@ public class Test {
 
         Member[] arr = new Member[5];
 
-        Member m1 = new Member(email: "abc@def.com", password: 1234, memberName: "김철수", gender: "남성", age: 30");
+        Member m1 = new Member("abc@def.com", "1234", "김철수", "남성", 30);
+        Member m2 = new Member("xyz@ghi.com", "4567", "김영희", "여성", 31);
 
+        arr[0] = m1;
+        arr[1] = m2;
+        arr[2] = new Member("ad", "d", "d", "d", 3);
+
+        System.out.println("m1 = " + m1);
+        System.out.println("m2 = " + m2);
+
+        Member[] members = {m1, m2};
+        /*
+           const members = [
+                {
+                    id: 1,
+                    memberName: '김철수',
+                },
+                {}
+           ];
+         */
+
+        System.out.println("=====================");
+        for (Member m : members) {
+            System.out.println(m.memberName);
+        }
+
+        System.out.println("=====================");
+
+        MemberRepository mr = new MemberRepository();
+        MemberView mv = new MemberView();
+
+        Member newMember = new Member("ddd@fff.com", "4321", "찰떡이", "남성", 12);
+
+        mr.addNewMember(newMember);
+
+        mv.showMembers();
 
 
     }
