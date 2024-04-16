76ded25b4df7e5374328708efe5483d7d1205b40
Jehoon <limjhoon8@gmail.com>
2024-04-16 09:56:50 +0900

    #파일 업데이트

---
+++ src/day06/member/Test.java
@@ -0,0 +1,16 @@
+package day06.member;
+
+import java.util.scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Member[] arr = new Member[5];
+
+        Member m1 = new Member(email: "abc@def.com", password: 1234, memberName: "김철수", gender: "남성", age: 30");
+
+
+
+    }
+}
