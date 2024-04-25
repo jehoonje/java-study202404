91a8caaf3cc6e58f8f761d5b7cbf714a4a09999d
Jehoon <limjhoon8@gmail.com>
2024-04-25 10:11:53 +0900

    #파일 업데이트

--- src/day12/stream/Mapping.java
+++ src/day12/stream/Mapping.java
@@ -2,6 +2,7 @@ package day12.stream;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static day12.stream.Menu.menuList;
@@ -66,6 +67,44 @@ public class Mapping {
                 .collect(Collectors.toList());
 
         simpleDishList.forEach((sd -> System.out.println(sd)));
+
+        /*
+            메뉴 목록에서 칼로리가 500칼로리보다 큰
+            음식들을 필터링한 다음에 음식의 이름과 타입만
+            추출해서 출력해주세요.
+
+            단, 타입은 MEAT의 경우 육류라고 저장
+            FISH는 어류라고 저장, OTHER는 기타라고 저장
+         */
+
+
+        System.out.println("===============================");
+        Set<DishDetail> dishDetailList = menuList.stream()
+                .filter(d -> d.getCalories() > 500)
+                .map(dish -> new DishDetail(dish))
+                .collect(Collectors.toSet());
+
+        System.out.println(dishDetailList);
+
+        System.out.println("==============================");
+
+        // 메뉴 목록에 있는 요리들의 총 칼로리 수 구하기
+        int sum = menuList.stream()
+                .mapToInt(d -> d.getCalories())
+                .sum();
+        System.out.println("sum = " + sum);
+
+        // 육류 메뉴의 평균 칼로리
+
+        double averageMeatCalories = menuList.stream()
+                .filter(d -> d.getType() == Dish.Type.MEAT)
+                .mapToInt(d -> d.getCalories())
+                .average()
+                .getAsDouble();
+
+        System.out.println("averageMeatCalories = " + averageMeatCalories);
+
+
 
     }
 }
